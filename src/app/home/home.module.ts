f4016a7e6b2d8876e7957cf30b40c7801fb0031e
Brayan Nedzib Sastoque Rangel <nedzib.sastoque@gmail.com>
2020-03-03 21:43:54 -0500

    home and a half task pages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+
 import { HomePage } from './home.page';
 
 @NgModule({
